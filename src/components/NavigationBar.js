f473b73ea24cfb60cfa4cf3c7f5af61046431560
arkstudiodevelopers <nicksonogm@gmail.com>
2024-09-08 19:26:56 +0300

    First commit with basic Header information

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavigationBar() {
+  return (
+    <div className='Navigation-bar'>
+        <h3>Image-Compressor</h3>
+        <ul>
+            <li><a>Portfolio</a></li>
+            <li><a>Github</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default NavigationBar
