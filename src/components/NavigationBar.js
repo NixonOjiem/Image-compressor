43481b99af3891c30903ab2784fd0c419070abc1
arkstudiodevelopers <nicksonogm@gmail.com>
2024-09-17 21:22:22 +0300

    All responsiveness done

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,8 +5,8 @@ function NavigationBar() {
     <div className='Navigation-bar'>
        <h3>Image compressor</h3>
         <ul className="List-items">
-            <li><a>Portfolio</a></li>
-            <li><a>Github</a></li>
+            <li><a href='https://myportfolio-cde82.web.app/'>Portfolio</a></li>
+            <li><a href='https://github.com/NixonOjiem'>Github</a></li>
         </ul>
     </div>
   )
